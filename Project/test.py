9e546c286f7bccf262163b5bce9c08a5c26e7ded
elymsyr <orhun868@gmail.com>
2024-09-03 13:11:41 +0300

    Remove test.py

--- Project/test.py
+++
@@ -1,7 +0,0 @@
-from iris_database import IrisSystem
-from iris_recognition import IrisRecognizer
-
-recognizer = IrisRecognizer()
-
-system = IrisSystem(db_path='Database/iris_db_syn_orb_0_100', recognizer=recognizer)
-system.compare_iris(image_tag_1='S6003S00', image_tag_2='S6003S07', show=True)
